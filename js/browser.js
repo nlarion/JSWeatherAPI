fa0cf3b47be208167b55444b3603d16e335de470
michael smith <michaelanthonysmith00@gmail.com>
2016-03-15 13:30:14 -0700

    test to see if jquery works.

--- js/browser.js
+++ js/browser.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+var apiKey = "64778dc3e2447a8db3666e2878a78b1a";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+  });
+});
